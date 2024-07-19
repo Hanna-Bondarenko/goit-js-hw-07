7a45d2114371be174ec97b6c53384752b94cf2af
Hanna Bondarenko <annaleshhuk@gmail.com>
2024-07-19 12:58:00 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const registerForm = document.querySelector('.login-form');
 
+registerForm.addEventListener('submit', handleSubmit => {
+  handleSubmit.preventDefault();
+});
+const { email, password } = handleSubmit.currentTarget.elements;
+const emailValue = email.value.trim();
+const passwordValue = password.value.trim();
+
+if (!emailValue || !passwordValue) {
+  alert('All form fields must be filled in');
+  return;
+}
+const formData = {
+  email: emailValue,
+  password: passwordValue,
+};
+console.log(formData);
+handleSubmit.currentTarget.reset();
